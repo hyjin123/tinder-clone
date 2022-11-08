703e2f62882441149f51b3b1dd43b409aa063c88
Sean <seanhoyeonjin@gmail.com>
2022-11-08 11:32:40 -0500

    setting up google auth

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -1,13 +1,15 @@
 import { View, Text } from "react-native";
 import React, { createContext, useContext } from "react";
+import * as Google from "expo-google-app-auth";
 
 const AuthContext = createContext({});
 
 // AuthProvider is passing down information as a global data layer, all children can access this data
 
 export const AuthProvider = ({ children }) => {
+  const signInWithGoogle = async () => {};
   return (
-    <AuthContext.Provider value={{ user: "sean" }}>
+    <AuthContext.Provider value={{ user: null }}>
       {children}
     </AuthContext.Provider>
   );
